a0547cc29b7d80e02e28ad0d41236d346d33c72c
ramyas-dev <kattaramyadevi@gmail.com>
2024-10-11 09:40:04 +0530

    Add test cases for all possible scenarios

--- tests/util/stringUtil.test.js
+++ tests/util/stringUtil.test.js
@@ -4,3 +4,21 @@ test("should return 0 if empty string", () => {
   const result = add("");
   expect(result).toBe(0);
 });
+
+test("should return sum of numbers if separated with comma", () => {
+  const result = add("1,2");
+  expect(result).toBe(3);
+});
+
+test("should return sum of numbers if separated with comma or \n also as delimiter", () => {
+  const result = add("1,2\n3,4\n5");
+  expect(result).toBe(15);
+});
+
+test("should throw expection when netgative value present", () => {
+  expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+});
+
+test("should throw expection when more than one negative value present", () => {
+  expect(() => add("1,-2,-3,4")).toThrow("negative numbers not allowed: -2,-3");
+});
