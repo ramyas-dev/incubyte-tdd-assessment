702036d5cac2f7ba7c8b4eb3e94dac2512675659
ramyas-dev <kattaramyadevi@gmail.com>
2024-10-11 09:44:07 +0530

    Update `Add` function to satisfy all use cases

--- src/util/stringUtil.js
+++ src/util/stringUtil.js
@@ -1,5 +1,17 @@
 function add(input) {
-  return 0;
+  if (!input) {
+    return 0;
+  } else {
+    const delimiter = /[,\n]/;
+    const numbers = input.split(delimiter).map(Number);
+    const negatives = numbers.filter((num) => num < 0);
+
+    if (negatives.length) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+    } else {
+      return numbers.reduce((sum, num) => sum + num, 0);
+    }
+  }
 }
 
 module.exports = { add };
